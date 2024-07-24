1de30b54c85b4b4e5bff0f8e42c1482d2da8fd61
Suhas Nidgundi <suhasnidgundi@gmail.com>
2024-07-24 17:33:31 +0530

    refactor: Update hero section greeting and add waving hand emoji

--- components/section/hero.jsx
+++ components/section/hero.jsx
@@ -11,14 +11,14 @@ const Hero = () => {
           Hi <span className="waving-hand">👋</span>
         </h1>
         <p>
-          Hey there! 👋💻 I'm a tech enthusiast on a journey to make my mark in
+          I&apos;m a tech enthusiast on a journey to make my mark in
           the world of computer engineering. Let me give you a quick snapshot of
-          where I've been, where I am, and where I'm headed.
+          where I&apos;ve been, where I am, and where I&apos;m headed.
         </p>
         <p>
           <strong>Past 🕰️</strong>
           <br />
-          In the past year, I've been busy laying the groundwork for my career.
+          In the past year, I&apos;ve been busy laying the groundwork for my career.
           I wrapped up my <strong>
             diploma in Information Technology
           </strong>{" "}
@@ -31,30 +31,30 @@ const Hero = () => {
         <p>
           <strong>Present 📚</strong>
           <br />
-          Currently, I'm hitting the books at{" "}
+          Currently, I&apos;m hitting the books at{" "}
           <strong>
             <a href="https://www.dypcoei.edu.in/">
               Dr. D. Y. Patil College of Engineering and Innovation
             </a>
           </strong>
-          , pursuing my bachelor's in Computer Engineering. It's challenging,
-          but I'm loving every bit of it. The late-night coding sessions, the
+          , pursuing my bachelor&apos;s in Computer Engineering. It&apos;s challenging,
+          but I&apos;m loving every bit of it. The late-night coding sessions, the
           eureka moments, and yes, even the occasional coffee-fueled debugging
-          marathons – they're all part of the adventure. ☕💡
+          marathons – they&apos;re all part of the adventure. ☕💡
         </p>
         <p>
           <strong>Future 🚀</strong>
           <br />
-          Looking ahead, I'm excited to dive deeper into the world of tech and
-          make a real impact. I'm aiming to blend my academic knowledge with
+          Looking ahead, I&apos;m excited to dive deeper into the world of tech and
+          make a real impact. I&apos;m aiming to blend my academic knowledge with
           hands-on experience to become a versatile and innovative computer
-          engineer. Whether it's developing cutting-edge software, exploring the
-          realms of AI, or contributing to groundbreaking projects, I'm ready to
+          engineer. Whether it&apos;s developing cutting-edge software, exploring the
+          realms of AI, or contributing to groundbreaking projects, I&apos;m ready to
           take on whatever challenges come my way.
         </p>
         <p>
-          So, that's me in a nutshell – a mix of accomplishments, ongoing
-          learning, and big dreams. Stick around, and let's see where this
+          So, that&apos;s me in a nutshell – a mix of accomplishments, ongoing
+          learning, and big dreams. Stick around, and let&apos;s see where this
           journey takes us! 🌟
         </p>
         <br />
