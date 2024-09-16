815075173c155da636094387dd9bd97c0d072f77
suhasnidgundi <suhasnidgundi@gmail.com>
2024-09-17 03:04:32 +0530

    refactor: Update aboutMe page content

--- app/aboutMe/page.js
+++ app/aboutMe/page.js
@@ -18,22 +18,19 @@ const page = () => {
         </Box>
         <Box>
           <Text ta="justify">
-            "On the other hand, we denounce with righteous indignation and
-            dislike men who are so beguiled and demoralized by the charms of
-            pleasure of the moment, so blinded by desire, that they cannot
-            foresee the pain and trouble that are bound to ensue; and equal
-            blame belongs to those who fail in their duty through weakness of
-            will, which is the same as saying through shrinking from toil and
-            pain. These cases are perfectly simple and easy to distinguish. In a
-            free hour, when our power of choice is untrammelled and when nothing
-            prevents our being able to do what we like best, every pleasure is
-            to be welcomed and every pain avoided. But in certain circumstances
-            and owing to the claims of duty or the obligations of business it
-            will frequently occur that pleasures have to be repudiated and
-            annoyances accepted. The wise man therefore always holds in these
-            matters to this principle of selection: he rejects pleasures to
-            secure other greater pleasures, or else he endures pains to avoid
-            worse pains."
+            I&apos;m Wen, a passionate Software Engineer with over 7 years of
+            experience in crafting exceptional digital experiences. Armed with a
+            Computer Science degree from McGill University, I specialize in
+            designing and implementing innovative full-stack web applications
+            and blockchain solutions. But what truly sets my heart on fire is
+            game development; it&apos;s where I transform my personal passion
+            into captivating virtual worlds. Beyond the world of technology,
+            I&apos;m a curious explorer. I delve into history, immerse myself in
+            the intricacies of politics, and navigate the complexities of
+            finance. When I&apos;m not busy coding, you&apos;ll find me on the
+            tennis court or badminton court, or perhaps creating harmonious
+            melodies through music. Join me on this exciting journey of
+            innovation, creativity, and endless possibilities.
           </Text>
         </Box>
       </Flex>
