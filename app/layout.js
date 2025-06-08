98fbbf0139edd07ec4c8cd821077c40fd0eaaa02
suhasnidgundi <suhasnidgundi@gmail.com>
2025-06-08 16:02:03 +0530

    fix: Add Clarity tracking code to the layout

--- app/layout.js
+++ app/layout.js
@@ -37,6 +37,18 @@ export default function RootLayout({ children }) {
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
         />
+        {/* Clarity tracking code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "rw8gqy3lxe");
+            `,
+          }}
+        />
       </head>
       <body>
         <RootStyleRegistry>
