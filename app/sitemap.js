45a05d379f2d8aa57e63b01b791f00a93ff12deb
suhasnidgundi <suhasnidgundi@gmail.com>
2025-06-08 15:53:28 +0530

    fix: Remove changeFrequency property from sitemap entries

--- app/sitemap.js
+++ app/sitemap.js
@@ -3,19 +3,16 @@ export default function sitemap() {
     {
       url: "https://suhasnidgundi.me",
       lastModified: new Date(),
-      changeFrequency: "yearly",
       priority: 1,
     },
     {
       url: "https://suhasnidgundi.me/about",
       lastModified: new Date(),
-      changeFrequency: "monthly",
       priority: 0.8,
     },
     {
       url: "https://suhasnidgundi.me/projects",
       lastModified: new Date(),
-      changeFrequency: "weakly",
       priority: 0.8,
     },
   ];
